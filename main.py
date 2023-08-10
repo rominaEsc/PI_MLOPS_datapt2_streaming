9f3718f61837d40d6ce6068acc5044e84b60e7f2
Romina <escudero.rominasoledad@gmail.com>
2023-08-10 00:02:36 -0300

    5. correccion en funciones

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def peliculas_duracion( pelicula: str ):
 
 # 3. función franquicia
 
-@app.get('/franquicia/{franquicia}')
+@app.get('/get_director/{nombre_director}')
 def franquicia(franquicia:str):
     '''Se ingresa la franquicia, retornando la cantidad de peliculas, ganancia total y promedio'''
     franquicia = franquicia.title().strip()
